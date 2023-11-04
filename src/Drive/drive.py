e54e4ca88e11cd86bcd7a29387b761ad2905946b
Swapnil Garg <swappyontube@gmail.com>
2023-11-04 18:09:11 +0530

    Added drive backend

---
+++ src/Drive/drive.py
@@ -0,0 +1,79 @@
+#How to use this:
+# get_folders() returns list of dicts. Each dict has ID and name of individual folder found in the drive
+# Show the user the folders on the GUI
+# Ask the user to select one and proceed
+# Ask user to select location where to download and provide folder name as well
+# call download_to_location(folder_id, save_directory) passing in the values as per user selection
+# Done :}
+
+import os
+import pickle
+import googleapiclient.discovery
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# Define the scopes for the Google Drive API
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+
+def get_drive_service(): #Returns the authenticated API service object 
+    creds = None
+
+    # The file token.pickle stores the user's access and refresh tokens
+    if os.path.exists('token.pickle'): #change if path where you want to save and search for token.pickle
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials, let the user log in
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token: #change if path where you want to save and search for token.pickle
+            pickle.dump(creds, token)
+
+    return googleapiclient.discovery.build('drive', 'v3', credentials=creds)
+
+# List folders in Google Drive (currently lists all folders and subfolders as distinct entities)
+def list_folders(service): #returns items (a list of dictionary objects, each dict has 'id' and 'name' representing ID and name of folders found) 
+    results = service.files().list(q="mimeType='application/vnd.google-apps.folder'", fields="files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print("No folders found.")
+    else:
+        return items
+
+# Download media files from a folder
+def download_media_files(service, folder_id, save_dir):
+    results = service.files().list(q=f"'{folder_id}' in parents", fields="files(id, name, mimeType)").execute()
+    files = results.get('files', []) 
+
+    if not files:
+        print("No media files found in the selected folder.")
+    else:
+        for file in files:
+            allowed_types = ['video/mpeg','audio/mpeg'] #CHANGE TO ADD MIME OF ALL ALLOWED FILE TYPES
+            if file['mimeType'] in allowed_types:
+                request = service.files().get_media(fileId=file['id'])
+                filename = os.path.join(save_dir, file['name'])
+                with open(filename, 'wb') as f:
+                    downloader = googleapiclient.http.MediaIoBaseDownload(f, request)
+                    done = False
+                    while not done:
+                        status, done = downloader.next_chunk()
+                        print(f"Downloading {item['name']}... {int(status.progress() * 100)}%")
+
+def get_folders():
+    service = get_drive_service()
+    return list_folders(service)
+
+#function to use in the application
+def download_to_location(folder_id, save_directory):
+    #provide this function with the folder_id and absolute path of the save_directory and it does the job 
+    service = get_drive_service()
+    download_media_files(service,folder_id,save_directory)
+    
