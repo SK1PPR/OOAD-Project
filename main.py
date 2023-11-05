0fdb68e874933b48b5790498f5f9f0787ab06e4e
Swapnil Garg <swappyontube@gmail.com>
2023-11-05 18:18:07 +0530

    added auto token removal upon closing

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
 		main_window=controller()
 		main_window.window.show()
 		sys.exit(app.exec_())
-  
 	except KeyboardInterrupt:
 		main_window.graceful_cleanup()
 		sys.exit(0)
--- src/Controller.py
+++ src/Controller.py
@@ -1,14 +1,21 @@
 from PyQt5 import QtWidgets, QtGui
 from src.Backend.playlist import playlist
 from src.Layouts.mainPlayer import main_player
-
+from PyQt5.QtCore import QCoreApplication
+import os
 
 class controller(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.set_main_player(parent)
+        QCoreApplication.instance().aboutToQuit.connect(self.cleanup)
     
     def set_main_player(self, parent):
         self.window = main_player(parent)
         
+    def cleanup(self):
+        try:
+            os.remove(os.path.join(os.path.dirname(__file__),'Drive','token.pickle'))
+        except:
+            print('token not found')
     
